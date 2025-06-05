5dc67099d9cc52aeca2fc1d9a8d6d8c1e16c91f5
Andrew <ajlouis892@gmail.com>
2025-06-05 11:42:32 -0400

    Backend

---
+++ app/src/main/java/com/example/peakplaysscorepredictor/backend/FootballAPIService.java
@@ -0,0 +1,24 @@
+package com.example.peakplaysscorepredictor.backend;
+
+// 1. API Service Interface
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+public interface FootballAPIService {
+    @GET("teams")
+    Call<APIPremierLeagueTeams> getTeams(
+            @Query("league") int league,
+            @Query("season") int season,
+            @Header("X-RapidAPI-Key") String rapidApiKey
+    );
+
+    @GET("players")
+    Call<APIResponseWrapper.apiWrapper> getPlayerStats(
+            @Query("team") int teamId,
+            @Query("season") int season,
+            @Query("league") int league,
+            @Header("x-apisports-key") String apiSportsKey
+    );
+}
